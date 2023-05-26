c616d014bd7a33d68e72da55c8bdeb3572eaad36
JuanMendozaOS <juanmendozasfa@gmail.com>
2023-05-26 00:22:04 -0400

    Change indend size 2 --> 4

--- src/BigO/On.java
+++ src/BigO/On.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class On {
 
-  private static void findNemo(String[] array) {
-    for (int i = 0; i < array.length; i++) {
-      if(array[i].equals("Nemo")){
-        System.out.println("Found NEMO!");
-      }
+    private static void findNemo(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals("Nemo")) {
+                System.out.println("Found NEMO!");
+            }
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    String[] everyone = new String[] {"Dory", "Bruce", "Marlin", "Nemo", "Gill", "Hank"};
-    String[] large = new String[10000];
-    Arrays.fill(large, "Nemo");
-    findNemo(large); // O(n) --> Linear
-  }
+    public static void main(String[] args) {
+        String[] everyone = new String[]{"Dory", "Bruce", "Marlin", "Nemo", "Gill", "Hank"};
+        String[] large = new String[10000];
+        Arrays.fill(large, "Nemo");
+        findNemo(large); // O(n) --> Linear
+    }
 }
